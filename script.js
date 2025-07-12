5048f2d15250189eded633227bb791b847356046
Muhammad-Kamal-Asif <mk9633273@gmail.com>
2025-07-12 17:35:50 +0500

    Uploading local code to Remote Repository

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("showMoreBtn").addEventListener("click", function () {
+  const hiddenCards = document.querySelectorAll(".cert-card.hidden");
+  hiddenCards.forEach(card => card.classList.remove("hidden"));
+  this.style.display = "none"; // hide the button
+});
+
+<script>
+  function toggleMenu() {
+    const nav = document.querySelector('nav ul');
+    nav.classList.toggle('show');
+  }
+</script>
