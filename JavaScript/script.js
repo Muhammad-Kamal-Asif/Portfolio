d11ab330bd61eb80cd4f32fda7c77bc806ab2b4f
Muhammad Kamal <mk9633273@gmail.com>
2025-07-21 02:39:25 -0700

    modified certificates page, modified script/styles

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,20 @@
-document.getElementById("showMoreBtn").addEventListener("click", function () {
-  const hiddenCards = document.querySelectorAll(".cert-card.hidden");
-  hiddenCards.forEach(card => card.classList.remove("hidden"));
-  this.style.display = "none"; // hide the button
+// FILTER BUTTONS
+const filterBtns = document.querySelectorAll(".filter-btn");
+const certCards = document.querySelectorAll(".cert-card");
+
+filterBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    // Remove 'active' class
+    filterBtns.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    const category = btn.getAttribute("data-filter");
+    certCards.forEach(card => {
+      if (category === "all" || card.classList.contains(category)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
 });
